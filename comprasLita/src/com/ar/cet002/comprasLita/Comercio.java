a527e39199fd0cd8f7d350a14e32700ea90cb9f8
jarquemaxi <39813161+jarquemaxi@users.noreply.github.com>
2018-06-07 20:19:41 -0300

    nuevas clases agregado

--- comprasLita/src/com/ar/cet002/comprasLita/Comercio.java
+++ comprasLita/src/com/ar/cet002/comprasLita/Comercio.java
@@ -5,10 +5,71 @@
  */
 package com.ar.cet002.comprasLita;
 
+import java.util.Date;
+import java.util.List;
+
 /**
  *
  * @author User
  */
 public class Comercio {
+    
+    private List<Producto> listaDeProductos;
+    private Nodo ubicacion;
+    private String duenio;
+    private Date horario;
+    private int calificacionPositiva;
+    private int calificacionNegativa;
+
+    public List<Producto> getListaDeProductos() {
+        return listaDeProductos;
+    }
+
+    public void setListaDeProductos(List<Producto> listaDeProductos) {
+        this.listaDeProductos = listaDeProductos;
+    }
+
+    public Nodo getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(Nodo ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getDuenio() {
+        return duenio;
+    }
+
+    public void setDuenio(String duenio) {
+        this.duenio = duenio;
+    }
+
+    public Date getHorario() {
+        return horario;
+    }
+
+    public void setHorario(Date horario) {
+        this.horario = horario;
+    }
+
+    public int getCalificacionPositiva() {
+        return calificacionPositiva;
+    }
+
+    public void setCalificacionPositiva(int calificacionPositiva) {
+        this.calificacionPositiva = calificacionPositiva;
+    }
+
+    public int getCalificacionNegativa() {
+        return calificacionNegativa;
+    }
+
+    public void setCalificacionNegativa(int calificacionNegativa) {
+        this.calificacionNegativa = calificacionNegativa;
+    }
+    
+    
+    
     
 }
--- comprasLita/src/com/ar/cet002/comprasLita/Producto.java
+++ comprasLita/src/com/ar/cet002/comprasLita/Producto.java
@@ -14,5 +14,23 @@ public class Producto {
     private String nombre;
     private double precio;
     private Comercio comercio;
-    
+
+    public Producto(String nombre, double precio, Comercio comercio) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.comercio = comercio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public Comercio getComercio() {
+        return comercio;
+    }
+       
 }
