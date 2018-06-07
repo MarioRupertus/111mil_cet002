217539fa21ecc2896c26ca7e2b642eacb6eff000
jarquemaxi <39813161+jarquemaxi@users.noreply.github.com>
2018-06-07 20:15:50 -0300

    nuevas clases

---
+++ comprasLita/src/com/ar/cet002/comprasLita/Producto.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ar.cet002.comprasLita;
+
+/**
+ *
+ * @author User
+ */
+public class Producto {
+    
+    private String nombre;
+    private double precio;
+    private Comercio comercio;
+    
+}
